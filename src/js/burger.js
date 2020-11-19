cf8d1d1dd786f7bb2b9f1833951870b0c181e430
Kamil Gajewski <kwaq89@interia.pl>
2020-11-19 23:29:56 +0100

    update scroll (add active link), changes size text in rwd

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,8 +1,24 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.navigation');
 
+const home = document.querySelector('.navigation ul li .home');
+const offer = document.querySelector('.navigation ul li .offer');
+const about = document.querySelector('.navigation ul li .about');
+const projects = document.querySelector('.navigation ul li .projects');
+const contact = document.querySelector('.navigation ul li .contact');
+
 burger.addEventListener("click", function () {
     burger.classList.toggle("on");
     nav.classList.toggle("showNav");
 })
 
+const goTo = () =>{
+    burger.classList.remove("on");
+    nav.classList.remove("showNav");
+}
+
+home.addEventListener('click', goTo);
+offer.addEventListener('click', goTo);
+about.addEventListener('click', goTo);
+projects.addEventListener('click', goTo);
+contact.addEventListener('click', goTo);
--- src/js/scroller.js
+++ src/js/scroller.js
@@ -1,5 +1,9 @@
 import SmoothScroll from "../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills";
+import Gumshoe from "../../node_modules/gumshoejs/dist/gumshoe.polyfills";
 
 const scroll = new SmoothScroll('a[href*="#"]', {
-    durationMax: 800
+    durationMax: 800,
+    updateURL: false
 });
+
+const spy = new Gumshoe('.navigation a');
